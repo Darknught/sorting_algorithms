fbeeefabc7be501a3069b121dc354952831dc1e5
Pibo Shongwe <piboman@yahoo.com>
2023-12-15 05:37:59 -0800

    u

---
+++ 2-selection_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+int temp;
+size_t i, j, min_index;
+	
+for (i = 0; i < size - 1; i++)
+{
+min_index = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_index])
+{
+min_index = j;
+}
+} 
+	if (min_index != i)
+	{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+		}
+	}
+}
