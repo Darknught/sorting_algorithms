019efa3e2b2113ee7cf9f0ed8ef01354d734b7e0
Pibo Shongwe <piboman@yahoo.com>
2023-12-15 16:29:32 +0200

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,14 +1,32 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-int partition(int *array, int low, int high) {
+/**
+ * partition - Partitions an array for the
+ * Quick Sort algorithm.
+ * @array: Pointer to the array to be partitioned.
+ * @low: Starting index of the partition.
+ * @high: Ending index of the partition.
+ *
+ * Return: The index of the pivot element.
+ */
+int partition(int *array, int low, int high)
+{
     int pivot = array[high];
-    int i = (low - 1);
+    int i = (low - 1), j;
 
-    for (int j = low; j <= high - 1; j++) {
+    for (j = low; j <= high - 1; j++)
+    {
         if (array[j] < pivot) {
             i++;
             swap(&array[i], &array[j]);
@@ -18,6 +36,14 @@ int partition(int *array, int low, int high) {
     return (i + 1);
 }
 
+
+/**
+ * quick_sort - Sorts an integer array
+ * in ascending order using Quick Sort.
+ * @array: Pointer to the integer array to be sorted.
+ * @low: Starting index of the array to be sorted.
+ * @high: Ending index of the array to be sorted.
+ */
 void quick_sort(int *array, int low, int high) {
     if (low < high) {
         int pi = partition(array, low, high);
