816be4ec78e6ab5cc550a226c1c337869f443149
Pibo Shongwe <piboman@yahoo.com>
2023-12-15 05:58:31 -0800

    u

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap - function to swap integers
+ * @x: first integer
+ * @y: second integer
+ *
+ */
+void swap(int *x, int *y)
+{
+	int tmp = *x;
+
+	*x = *y;
+	*y = tmp;
+}
+
+/**
  * selection_sort - Sorts an integer array in
  * ascending order using the selection sort algorithm.
  * @array: Pointer to the integer array to be sorted.
