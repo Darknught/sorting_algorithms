7907a61e65ac7a246fceb09fa2b8bf83532eea2e
Pibo Shongwe <piboman@yahoo.com>
2023-12-15 15:38:34 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,7 +4,7 @@ void selection_sort(int *array, size_t size)
 {
 int temp;
 size_t i, j, min_index;
-	
+
 for (i = 0; i < size - 1; i++)
 {
 min_index = i;
@@ -15,11 +15,11 @@ if (array[j] < array[min_index])
 min_index = j;
 }
 } 
-	if (min_index != i)
-	{
-			temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
-		}
-	}
+if (min_index != i)
+{
+temp = array[i];
+array[i] = array[min_index];
+array[min_index] = temp;
+}
+}
 }
