d09631c0e1b2030f6ffa0ac2ad64aaa3e851ea2a
Darknught <hassino66@yahoo.com>
2023-12-17 15:12:16 +0000

    code

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,27 +1,36 @@
 #include "sort.h"
 
 /**
- * swap - Swaps two nodes in a doubly linked list
+ * swap_n - Swaps two nodes in a doubly linked list
  * @list: Pointer to the head of the linked list
  * @node1: first node
  * @node2: second node
  */
-void swap_n(listint_t **list, listint_t *node1, listint_t *node2)
+void swap_n(listint_t **list, listint_t **node1, listint_t **node2)
 {
-	if (node1->prev)
-		node1->prev->next = node2;
-	else
-		*list = node2;
+	listint_t *temp;
 
-	if (node2->next)
-		node2->next->prev = node1;
+	if (*node1 == NULL || *node2 == NULL)
+		return;
 
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node1->prev = node2;
-	node2->next = node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = *node2;
 
-	print_list(*list);
+	if ((*node2)->next != NULL)
+		(*node2)->next->prev = *node1;
+
+	temp = (*node1)->prev;
+	(*node1)->prev = (*node2)->prev;
+	(*node2)->prev = temp;
+
+	temp = (*node1)->next;
+	(*node1)->next = (*node2)->next;
+	(*node2)->next = temp;
+
+	if ((*node1)->prev == NULL)
+		*list = *node2;
+	if ((*node2)->next == NULL)
+		*list = *node1;
 }
 
 /**
@@ -31,35 +40,36 @@ void swap_n(listint_t **list, listint_t *node1, listint_t *node2)
  */
 void cocktail_sort_list(listint_t **list)
 {
-	int value = 1;
+	int value;
 	listint_t *current;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	while (value)
-	{
+	do {
 		value = 0;
 		for (current = *list; current->next != NULL;
 				current = current->next)
 		{
 			if (current->n > current->next->n)
 			{
-				swap_n(list, current, current->next);
+				swap_n(list, &current, &current->next);
 				value = 1;
+				print_list(*list);
 			}
 		}
 		if (!value)
 			break;
 
 		value = 0;
-		for(; current->prev != NULL; current = current->prev)
+		for (; current->prev != NULL; current = current->prev)
 		{
-			if (current->n < current->prev->n)
+			if (current->prev->n > current->n)
 			{
-				swap_n(list, current->prev, current);
+				swap_n(list, &current->prev, &current);
 				value = 1;
+				print_list(*list);
 			}
 		}
-	}
+	} while (value);
 }
