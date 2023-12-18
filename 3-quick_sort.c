4c8beaf24abae356925c3a2b77f9f959641d209a
Pibo Shongwe <piboman@yahoo.com>
2023-12-17 17:24:22 -0800

    u

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,7 @@
 #include "sort.h"
 
+size_t myVal = 0;
+
 /**
  * swap - Swaps two integers.
  * @a: Pointer to the first integer.
@@ -8,6 +10,7 @@
 void swap(int *a, int *b)
 {
 int temp = *a;
+
 *a = *b;
 *b = temp;
 }
@@ -18,24 +21,30 @@ int temp = *a;
  * @array: Pointer to the array to be partitioned.
  * @low: Starting index of the partition.
  * @high: Ending index of the partition.
- * @size: size
  * Return: The index of the pivot element.
  */
-int partition(int *array, int low, int high, size_t size)
+int partition(int *array, int low, int high)
 {
 int pivot = array[high];
-int i = (low - 1), j;
+int i = low - 1, j;
 
 for (j = low; j <= high - 1; j++)
 {
 if (array[j] < pivot)
 {
 i++;
+if (array[i] != array[j])
+{
 swap(&array[i], &array[j]);
+print_array(array, myVal);
 }
 }
+}
+if (array[i + 1] != array[high])
+{
 swap(&array[i + 1], &array[high]);
-print_array(array, size);
+print_array(array, myVal);
+}
 return (i + 1);
 }
 
@@ -44,17 +53,16 @@ return (i + 1);
  * @array: array of items
  * @low: Starting index of the array to be sorted.
  * @high: Ending index of the array to be sorted.
- * @size: size
  */
-void quick_sort_helper(int *array, int low, int high, size_t size)
+void quick_sort_helper(int *array, int low, int high)
 {
 int pi;
 
 if (low < high)
 {
-pi = partition(array, low, high, size);
-quick_sort_helper(array, low, pi - 1, size);
-quick_sort_helper(array, pi + 1, high, size);
+pi = partition(array, low, high);
+quick_sort_helper(array, low, pi - 1);
+quick_sort_helper(array, pi + 1, high);
 }
 }
 
@@ -62,9 +70,9 @@ quick_sort_helper(array, pi + 1, high, size);
  * quick_sort - Sorts an integer array
  * in ascending order using Quick Sort.
  * @array: Pointer to the integer array to be sorted.
- * @size: size
  */
 void quick_sort(int *array, size_t size)
 {
-quick_sort_helper(array, 0, size - 1, size);
+myVal = size;
+quick_sort_helper(array, 0, size - 1);
 }
