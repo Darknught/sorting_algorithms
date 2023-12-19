3a7c2117f4ca7e76dc3222e990d003b4645f52d0
Pibo Shongwe <piboman@yahoo.com>
2023-12-19 08:18:08 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -30,7 +30,8 @@ int partition(int *array, int low, int high, size_t size);
 void quick_sort_helper(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
-void swap_n(listint_t **list, listint_t *node1, listint_t *node2);
+void swap_nodes_ahead(listint_t **list, listint_t *node1, listint_t *node2);
+void swap_nodes_behind(listint_t **list, listint_t *node1, listint_t *node2);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 
