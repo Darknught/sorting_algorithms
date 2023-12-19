888297a7e59623f0f24b562abbc0b28f80c84228
Pibo Shongwe <piboman@yahoo.com>
2023-12-19 08:19:44 +0200

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,42 +1,57 @@
 #include "sort.h"
 
 /**
- * swap_n - Swaps two nodes in a doubly linked list
+ * swap_nodes_behind - Swaps two nodes in a doubly linked list
  * @list: Pointer to the head of the linked list
  * @node1: first node
  * @node2: second node
  */
-void swap_n(listint_t **list, listint_t *node1, listint_t *node2)
+void swap_nodes_behind(listint_t **list, listint_t *node1, listint_t *node2)
 {
-	listint_t *temp_prev, *temp_next;
+if (node1 == NULL || node2 == NULL)
+return;
 
-	if (node1 == NULL || node2 == NULL)
-		return;
+if (node1->prev != NULL)
+node1->prev->next = node2;
+else
+*list = node2;
 
-	if (node1->prev != NULL)
-		node1->prev->next = node2;
-	if (node2->next != NULL)
-		node2->next->prev = node1;
+if (node2->next != NULL)
+node2->next->prev = node1;
 
-	if (node1->next != NULL && node1->next != node2)
-		node1->next->prev = node2;
-	if (node2->prev != NULL && node2->prev != node1)
-		node2->prev->next = node1;
-	temp_prev = node1->prev;
-	temp_next = node1->next;
-
-	node1->prev = node2->prev;
-	node1->next = node2->next;
-
-	node2->prev = temp_prev;
-	node2->next = temp_next;
-
-	if (node1->prev == NULL)
-		*list = node2;
-
-	if (node2->next == NULL)
-		*list = node1;
+node1->next = node2->next;
+node2->prev = node1->prev;
+node1->prev = node2;
+node2->next = node1;
 }
+
+/**
+ * swap_nodes_ahead - Swaps two nodes in a doubly linked list
+ * @list: Pointer to the head of the linked list
+ * @node1: first node
+ * @node2: second node
+ */
+void swap_nodes_ahead(listint_t **list, listint_t *node1, listint_t *node2)
+{
+if (node1 == NULL || node2 == NULL)
+return;
+
+if (node1->next != NULL)
+node1->next->prev = node2;
+else
+return;
+
+if (node2->prev != NULL)
+node2->prev->next = node1;
+else
+*list = node1;
+
+node1->prev = node2->prev;
+node2->next = node1->next;
+node1->next = node2;
+node2->prev = node1;
+}
+
 
 /**
  * cocktail_sort_list - Sort a dobly linked list using the Cocktail Shaker sort
@@ -58,7 +73,7 @@ void cocktail_sort_list(listint_t **list)
 		{
 			if (current->n > current->next->n)
 			{
-				swap_n(list, current, current->next);
+				swap_nodes_ahead(list, current, current->next);
 				print_list(*list);
 				value = 1;
 			}
@@ -71,7 +86,7 @@ void cocktail_sort_list(listint_t **list)
 		{
 			if (current->prev != NULL && current->prev->n > current->n)
 			{
-				swap_n(list, current->prev, current);
+				swap_nodes_behind(list, current->prev, current);
 				print_list(*list);
 				value = 1;
 			}
